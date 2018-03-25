ac1dd47baefd25d7f171c17fc8b037e11323800d
kverko <slawomirr@gmail.com>
2018-03-25 12:46:05 +0200

    adding db to imports

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Configuration(object):
     SQLALCHEMY_DATABASE_URI = \
         'postgresql://postgres:postgres@localhost:5432/blog_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app, db
+import models
 import views
+
 
 if __name__ == '__main__':
     app.run()
