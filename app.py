6fcfb5ecc6dd99de54a82a22d42b9556411d5126
kverko <slawomirr@gmail.com>
2018-03-28 20:23:25 +0200

    views, templates and search for entries

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
@@ -6,3 +8,8 @@ from config import Configuration
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
