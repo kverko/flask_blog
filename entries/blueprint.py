375473a2f2367f351c8a3f6ecc640e9ebe3d0d3d
kverko <slawomirr@gmail.com>
2018-04-05 22:36:39 +0200

    deleting entry

--- entries/blueprint.py
+++ entries/blueprint.py
@@ -41,12 +41,12 @@ def create():
 
 @entries.route('/<slug>/')
 def detail(slug):
-    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    entry = get_entry_or_404(slug)
     return render_template('entries/detail.html', entry=entry)
 
 @entries.route('/<slug>/edit/', methods=['GET', 'POST'])
 def edit(slug):
-    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    entry = get_entry_or_404(slug)
     if request.method == 'POST':
         form = EntryForm(request.form, obj=entry)
         if form.validate():
@@ -58,14 +58,29 @@ def edit(slug):
         form = EntryForm(obj=entry)
     return render_template('entries/edit.html', entry=entry, form=form)
 
-
+@entries.route('/<slug>/delete/'. methods=['GET', 'POST'])
+def delete(slug):
+    entry = get_entry_or_404(slug)
+    if request.method == 'POST':
+        entry.status = Entry.STATUS_DELETED
+        db.session.add(entry)
+        db.session.commit()
+        return redirect(url_for('entries.index'))
+    return render_template('entries/delete.html', entry=entry)
 
 def entry_list(template, query, **context):
+    valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
+    query = query.filter(Entry.status.in_(valid_statuses))
     search = request.args.get('q')
-    query = query.filter(Entry.status == Entry.STATUS_PUBLIC)
     if search:
         query = query.filter(
             (Entry.body.contains(search)) |
             (Entry.title.contains(search))
         )
     return object_list(template, query, **context)
+
+def get_entry_or_404(slug):
+    valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
+    return Entry.query.filter(
+        (Entry.slug == slug) &
+        (Entry.status.in_(valid_statuses))).first_or_404())
